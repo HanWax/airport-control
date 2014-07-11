e8270ac75e8ab420f9923b99df425e4145cab444
Hannah Waxman <hannahwaxman@Hannahs-MacBook-Pro.local>
2014-07-11 16:30:11 +0100

    Added core functionality to the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,32 @@
+DEFAULT_CAPACITY = 50 
+
 class Airport
 
-	def airport
-		@airport ||= []
+	def initialize(capacity: DEFAULT_CAPACITY)
+		@capacity = capacity 
+		@airport  = []
 	end
+
+	def capacity
+      	DEFAULT_CAPACITY
+    end
+
+    def planes
+    	@planes ||= []
+    end
+
+	def ground(plane)
+		raise 'Airport full, keep on circling' if full? 
+    	planes << plane 
+    end
+
+    def clear(plane)
+    	planes.delete(plane)
+    end
+
+    def full?
+    	planes.count == capacity 
+    end
+
+
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,12 +2,41 @@ require 'airport'
 require 'plane'
 
 describe Airport do
+  let(:airport)       { Airport.new                        }
+  let(:grounded_plane)   {double :airplane, status: 'Grounded'}
+  let(:flying_plane)     {double :airplane, status: 'Flying'  }
+  let(:plane)            {double :airplane} 
 
-  let(:airport) { Airport.new     }
-  let(:plane)   { double :airplane}
+  context 'taking off and landing' do 
 
-  context 'Airport upon initialisation' do 
+    it "should initialise with a set capacity" do
+      expect(airport.capacity).to eq 50
+    end
+
+    it 'should be empty when created' do 
+      expect(airport.planes).to be_empty
+    end
+
+    it 'should ground a plane' do
+      airport.ground(plane)
+      expect(airport.planes).to eq([plane])
+    end
+
+    it 'should clear a plane' do 
+      airport.clear(plane)
+      expect(airport.planes).to be_empty
+    end
+
+  end
+
+  context 'traffic control' do 
+
+    it 'should not ground a plane when full' do
+      50.times{airport.ground(plane)}
+      expect{ airport.ground(plane) }.to raise_error(RuntimeError)
+    end
+
   end 
-
-
+  
 end
+
